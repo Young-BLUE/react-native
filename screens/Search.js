15daf58109fa61f0b9f436517b0191a4c5287d71
Young-BLUE <youngblue.coding@gmail.com>
2022-12-19 22:15:19 +0900

    Search 하면 HList로 결과 나타내기

--- screens/Search.js
+++ screens/Search.js
@@ -2,6 +2,8 @@ import styled from "styled-components/native";
 import {useState} from "react";
 import {useQuery} from "react-query";
 import {moviesAPI, tvApi} from "../api";
+import Loader from "../components/Loader";
+import HList from "../components/HList";
 
 const Container = styled.ScrollView`
 
@@ -12,19 +14,23 @@ const SearchBar = styled.TextInput`
   padding: 10px 15px;
   border-radius: 15px;
   width: 90%;
-  margin: 10px auto;
+  margin: 10px auto 40px auto;
 `
 
 const Search = () => {
     const [query, setQuery] = useState("");
-    const {isLoading: moviesLoading, data: moviesData, refetch: searchMovies} = useQuery(["searchMovies", query], moviesAPI.search, {
+    const {
+        isLoading: moviesLoading,
+        data: moviesData,
+        refetch: searchMovies
+    } = useQuery(["searchMovies", query], moviesAPI.search, {
         enabled: false,
     });
     const {isLoading: tvLoading, data: tvData, refetch: searchTv} = useQuery(
         ["searchTv", query],
         tvApi.search, {
-        enabled: false,
-    });
+            enabled: false,
+        });
     const onChangeText = (text) => {
         setQuery(text);
     }
@@ -43,6 +49,9 @@ const Search = () => {
             onChangeText={onChangeText}
             onSubmitEditing={onSubmit}
         />
+        {moviesLoading || tvLoading ? <Loader/> : null}
+        {moviesData ? <HList title={"Movie Results"} data={moviesData.results} /> : null}
+        {tvData ? <HList title={"Tv Results"} data={tvData.results} /> : null}
     </Container>);
 };
 
