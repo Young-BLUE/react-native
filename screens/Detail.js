ad697df980cf4d704e52e65e968ea3ecff9c8d63
Young-BLUE <youngblue.coding@gmail.com>
2022-12-25 23:18:53 +0900

    크리스마스 커밋! - Expo LinearGradient 를 사용해서 Detail에서 뒷 배경 (backdrop) 이미지에 그래디언트를 넣었음. - 그래디언트 최소 지원이 ios 13.0 부터라서 xcode 설정 통해서 build ios minimum version 수정해서 빌드 성공
    Merry Christmas! Ho Ho Ho!

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,11 +1,42 @@
 import React, {useEffect} from "react";
 import styled from "styled-components/native";
-import {Text, View} from "react-native";
+import {Dimensions, StyleSheet, Text, View} from "react-native";
 import Poster from "../components/Poster";
+import {makeImagePath} from "../Utils";
+import {LinearGradient} from "expo-linear-gradient";
+import {BLACK_COLOR} from "../colors";
+
+const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${props => props.theme.mainBgColor};
 `
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+`
+
+const Background = styled.Image``
+
+const Column = styled.View`
+  flex-direction: row;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  width: 70%;
+  margin-left: 20px;
+  font-weight: 500;
+`;
+
+const OverView = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0px 20px;
+`;
 
 const Detail = ({
                     navigation: {setOptions},
@@ -13,12 +44,26 @@ const Detail = ({
                 }) => {
     useEffect(() => {
         setOptions({
-            title: 'original_title' in params ? params.original_title : params.original_name,
+            title: 'original_title' in params ? "Movie" : "TV Show",
         })
     }, [])
     return (
         <Container>
-            <Poster path={params.poster_path || ''} />
+            <Header>
+                <Background style={StyleSheet.absoluteFill} source={{
+                    uri: makeImagePath(params.backdrop_path,
+                        'w500')
+                }}/>
+                <LinearGradient
+                    colors={['transparent', BLACK_COLOR]}
+                    style={StyleSheet.absoluteFill}
+                />
+                <Column>
+                <Poster path={params.poster_path || ''}/>
+                    <Title>{'original_title' in params ? params.original_title : params.original_name}</Title>
+                </Column>
+            </Header>
+            <OverView>{params.overview}</OverView>
         </Container>
     )
 }
